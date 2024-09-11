fef0955cb345d3ff945e73f0c14a3355db17ea75
Prabhjot Singh <prabhjotsingh394@gmail.com>
2024-09-11 09:47:22 +0530

    create REST implementation

---
+++ src/main/java/com/gcorporation/cruddemo/rest/EmployeeRestController.java
@@ -0,0 +1,26 @@
+package com.gcorporation.cruddemo.rest;
+
+import com.gcorporation.cruddemo.dao.EmployeeDAO;
+import com.gcorporation.cruddemo.entity.Employee;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+
+    private EmployeeDAO employeeDAO;
+
+    public EmployeeRestController(EmployeeDAO theEmployeeDAO) {
+        employeeDAO = theEmployeeDAO;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> findAll() {
+        return employeeDAO.findAll();
+    }
+
+}

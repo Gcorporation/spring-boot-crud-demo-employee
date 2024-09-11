a9db284ef7d565562ce3c7324d73d10415b0d68c
Prabhjot Singh <prabhjotsingh394@gmail.com>
2024-09-11 10:55:00 +0530

    get single employee by ID

--- src/main/java/com/gcorporation/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/gcorporation/cruddemo/rest/EmployeeRestController.java
@@ -4,6 +4,7 @@ import com.gcorporation.cruddemo.dao.EmployeeDAO;
 import com.gcorporation.cruddemo.entity.Employee;
 import com.gcorporation.cruddemo.service.EmployeeService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,16 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAll() {
         return employeeService.findAll();
+    }
+
+    // add mapping for GET /employees/{employeeId}
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+        Employee theEmployee = employeeService.findById(employeeId);
+        if(theEmployee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+        return theEmployee;
     }
 
 }
